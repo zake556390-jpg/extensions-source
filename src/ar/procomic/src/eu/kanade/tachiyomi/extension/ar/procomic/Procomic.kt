f29e739efa99585f8075ddbe4c84849adf253025
zake556390-jpg <zake556390@gmail.com>
2026-05-11 18:01:19 +0200

    Update Procomic.kt

--- src/ar/procomic/src/eu/kanade/tachiyomi/extension/ar/procomic/Procomic.kt
+++ src/ar/procomic/src/eu/kanade/tachiyomi/extension/ar/procomic/Procomic.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.extension.ar.procomic
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.*
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import okhttp3.Request
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
@@ -12,48 +13,51 @@ class Procomic : ParsedHttpSource() {
     override val lang = "ar"
     override val supportsLatest = true
 
-    // الشعبيات (القائمة الرئيسية)
-    override fun popularMangaRequest(page: Int) = GET("$baseUrl/popular?page=$page", headers)
-    override fun popularMangaSelector() = "div.manga-card"
+    // إعدادات العرض الشعبي
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/popular?page=$page", headers)
+    override fun popularMangaSelector() = "div.manga-card, div.post-item" // جلب أكثر من احتمال لشكل التصميم
     override fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
         setUrlWithoutDomain(element.select("a").attr("href"))
-        title = element.select("h3.title").text()
+        title = element.select("h3, .title").text()
         thumbnail_url = element.select("img").attr("abs:src")
     }
-    override fun popularMangaNextPageSelector() = "a.next-page"
+    override fun popularMangaNextPageSelector() = "a.next-page, .pagination a.next"
 
-    // أحدث الإصدارات
-    override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/latest?page=$page", headers)
+    // أحدث الفصول
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/latest?page=$page", headers)
     override fun latestUpdatesSelector() = popularMangaSelector()
     override fun latestUpdatesFromElement(element: Element) = popularMangaFromElement(element)
     override fun latestUpdatesNextPageSelector() = popularMangaNextPageSelector()
 
-    // البحث
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList) = GET("$baseUrl/search?q=$query&page=$page", headers)
+    // البحث عن Keyboard Immortal وغيرها
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        return GET("$baseUrl/search?q=$query&page=$page", headers)
+    }
     override fun searchMangaSelector() = popularMangaSelector()
     override fun searchMangaFromElement(element: Element) = popularMangaFromElement(element)
     override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
 
     // تفاصيل المانجا
-    override fun mangaDetailsParse(document: Document) = SManga.create().apply {
-        title = document.select("h1.manga-title").text()
-        description = document.select("div.summary-text").text()
-        thumbnail_url = document.select("div.manga-cover img").attr("abs:src")
+    override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
+        title = document.select("h1").text()
+        description = document.select(".summary-text, .description").text()
+        thumbnail_url = document.select(".manga-cover img, .post-thumbnail img").attr("abs:src")
     }
 
-    // قائمة الفصول
-    override fun chapterListSelector() = "li.chapter-item"
-    override fun chapterFromElement(element: Element) = SChapter.create().apply {
+    // الفصول
+    override fun chapterListSelector() = "li.chapter-item, .wp-manga-chapter"
+    override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
         setUrlWithoutDomain(element.select("a").attr("href"))
-        name = element.select("span.chapter-name").text()
+        name = element.select(".chapter-name, a").text()
     }
 
-    // صور الفصل
+    // الصفحات
     override fun pageListParse(document: Document): List<Page> {
-        return document.select("div.reading-content img").mapIndexed { i, img ->
+        return document.select(".reading-content img, .page-break img").mapIndexed { i, img ->
             Page(i, "", img.attr("abs:src"))
         }
     }
 
     override fun imageUrlParse(document: Document) = throw UnsupportedOperationException("Not used")
 }
+
